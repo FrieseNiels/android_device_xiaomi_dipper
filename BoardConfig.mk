4a324fc1b7c57bfb3329c3f6e9f60798bd7ba844
argraur <argraur@gmail.com>
2018-09-30 15:40:59 +0000

    BoardConfig: build boot image, define partition sizes
    And many, many more
    
    Signed-off-by: argraur <argraur@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -26,6 +26,16 @@ TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := generic
 
+# Kernel
+BOARD_KERNEL_BASE := 0x00000000
+BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 earlycon=msm_geni_serial,0xA84000 androidboot.hardware=qcom androidboot.console=ttyMSM0 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 service_locator.enable=1 swiotlb=2048 androidboot.configfs=true firmware_class.path=/vendor/firmware_mnt/image loop.max_part=7
+BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
+BOARD_KERNEL_PAGESIZE := 4096
+BOARD_KERNEL_TAGS_OFFSET := 0x00000100
+BOARD_RAMDISK_OFFSET := 0x01000000
+NEED_KERNEL_MODULE_SYSTEM := true
+TARGET_KERNEL_ARCH := arm64
+
 # Enable 64-bits binder
 TARGET_USES_64_BIT_BINDER := true
 
@@ -36,13 +46,19 @@ BOARD_VNDK_VERSION := current
 TARGET_SYSTEM_PROP := build/make/target/board/treble_system.prop
 BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED := true
 
-# Bootloader, kernel and recovery are not part of generic AOSP image
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := sdm845
 TARGET_NO_BOOTLOADER := true
-TARGET_NO_KERNEL := true
 
-# system.img is always ext4 with sparse option
-# GSI also includes make_f2fs to support userdata parition in f2fs
-# for some devices
+# Partitions
+BOARD_BOOTIMAGE_PARTITION_SIZE := 67108864
+BOARD_CACHEIMAGE_PARTITION_SIZE := 268435456
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 67108864
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 3221225472
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 57453555712
+
+BOARD_FLASH_BLOCK_SIZE := 262144 # (BOARD_KERNEL_PAGESIZE * 64)
+
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
 TARGET_USERIMAGES_SPARSE_EXT_DISABLED := false
@@ -57,6 +73,13 @@ BOARD_USES_METADATA_PARTITION := true
 # Generic AOSP image does NOT support HWC1
 TARGET_USES_HWC2 := true
 
+# Lights
+TARGET_PROVIDES_LIBLIGHT := true
+
+# Recovery
+TARGET_RECOVERY_FSTAB := device/xiaomi/dipper/rootdir/etc/fstab.qcom
+TARGET_RECOVERY_PIXEL_FORMAT := "BGRA_8888"
+
 # NFC
 TARGET_USES_NQ_NFC := false
 
@@ -69,16 +92,29 @@ BOARD_BUILD_DISABLED_VBMETAIMAGE := true
 # Dipper properties
 TARGET_SYSTEM_PROP := device/xiaomi/dipper/system.prop $(TARGET_SYSTEM_PROP)
 
-# SEPolicy Private
+# SEPolicy
 BOARD_PLAT_PRIVATE_SEPOLICY_DIR += device/xiaomi/dipper/sepolicy/private
+
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += device/qcom/sepolicy/private
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += device/qcom/sepolicy/public
 
 # bdroid configuration
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/xiaomi/dipper/bluetooth
 
-TARGET_EXFAT_DRIVER := exfat
+# Camera
+TARGET_USES_QTI_CAMERA_DEVICE := true
 
-# System image size
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 3221225472
+# Charger Mode
+BOARD_CHARGER_ENABLE_SUSPEND := true
+
+# Filesystem
+TARGET_FS_CONFIG_GEN := device/xiaomi/dipper/config.fs
+
+# Graphics
+NUM_FRAMEBUFFER_SURFACE_BUFFERS := 2
+TARGET_USES_HWC2 := true
+
+TARGET_EXFAT_DRIVER := exfat
 
 #SELINUX_IGNORE_NEVERALLOWS := true
 
